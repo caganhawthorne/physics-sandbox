d5b07e386bf83d23f12b38ab3d37bbf83ffb525b
Connor <cjp.pan@gmail.com>
2015-07-13 14:27:30 -0500

    lol jk changed gravity back

--- Physics Sandbox/PlayModeViewController.swift
+++ Physics Sandbox/PlayModeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class PlayModeViewController: UIViewController, UICollisionBehaviorDelegate {
     
@@ -14,7 +15,8 @@ class PlayModeViewController: UIViewController, UICollisionBehaviorDelegate {
     var allObjects : [Item] = []
     var dynObjects : [UIDynamicItem] = []
     var collisionBehavior = UICollisionBehavior()
-    
+    var motionManager = CMMotionManager()
+    var gravity : UIGravityBehavior!
 
     
     override func viewDidLoad() {
@@ -34,15 +36,21 @@ class PlayModeViewController: UIViewController, UICollisionBehaviorDelegate {
         dynamicAnimator.addBehavior(collisionBehavior)
         for index in allObjects {
             collisionBehavior.addItem(index)
+            
         }
-        
+        gravity = UIGravityBehavior(items: allObjects)
+        dynamicAnimator.addBehavior(gravity)
+        /*
         let pushBehavior = UIPushBehavior(items: allObjects, mode: UIPushBehaviorMode.Continuous)
         pushBehavior.pushDirection = CGVectorMake(0, 4.71238898038)
         pushBehavior.magnitude = 5
         dynamicAnimator.addBehavior(pushBehavior)
-        
+        */
+    
         
         print("\(allObjects.count)")
+        
+        //motionManager.startAccelerometerUpdatesToQueue(<#T##queue: NSOperationQueue##NSOperationQueue#>, withHandler: gravityUpdated(CMDeviceMotion, error: NSError.self))
        
     }
     @IBAction func onStuffBeingDragged(sender: UIPanGestureRecognizer) {
@@ -50,6 +58,37 @@ class PlayModeViewController: UIViewController, UICollisionBehaviorDelegate {
         let translation = sender.translationInView(self.view!)
         sender.view!.center = CGPointMake(sender.view!.center.x + translation.x, sender.view!.center.y + translation.y)
         sender.setTranslation(CGPointZero, inView: self.view!)
+    }
+    
+    func gravityUpdated(motion: CMDeviceMotion!, error: NSError!) {
+        if (error != nil) {
+            NSLog("\(error)")
+        }
+        
+        let grav : CMAcceleration = motion.gravity;
+        
+        let x = CGFloat(grav.x);
+        let y = CGFloat(grav.y);
+        var p = CGPointMake(x,y)
+        
+        // Have to correct for orientation.
+        var orientation = UIApplication.sharedApplication().statusBarOrientation;
+        
+        if orientation == UIInterfaceOrientation.LandscapeLeft {
+            var t = p.x
+            p.x = 0 - p.y
+            p.y = t
+        } else if orientation == UIInterfaceOrientation.LandscapeRight {
+            var t = p.x
+            p.x = p.y
+            p.y = 0 - t
+        } else if orientation == UIInterfaceOrientation.PortraitUpsideDown {
+            p.x *= -1
+            p.y *= -1
+        }
+        
+        var v = CGVectorMake(p.x, 0 - p.y);
+        gravity.gravityDirection = v;
     }
 
 }
